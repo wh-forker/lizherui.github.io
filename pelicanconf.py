42c658f1c813c7abaaf9daa914bcb58238f563c9
lizherui <lzrak47m4a1@gmail.com>
2013-08-01 14:54:16 +0800

    ci by lizherui

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'lizherui'
+SITENAME = u"lizherui's blog"
+SITEURL = 'http://lizherui.github.io'
+TIMEZONE = 'Asia/Shanghai'
+THEME = "tuxlite_tbs"
+DEFAULT_LANG = u'zh'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+GITHUB_URL = "https://github.com/lizherui"
+GOOGLE_ANALYTICS = "UA-42648273-1"
+DISQUS_SITENAME = "lizherui"
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+ARCHIVES_URL = "archives.html"
+RELATIVE_URLS = True
+DEFAULT_PAGINATION = 5
+
+# Blogroll
+LINKS =  (('Pelican', 'http://getpelican.com/'),
+          ('Python.org', 'http://python.org/'),
+          ('Jinja2', 'http://jinja.pocoo.org/'),
+          )
+
+# Social widget
+SOCIAL = (('rss', 'http://lizherui.github.io/feeds/all.rss.xml'),
+          ('github', 'https://github.com/lizherui'),
+          ('weibo', 'http://weibo.com/lzrm4a1'),
+          ('zhihu', 'http://www.zhihu.com/people/li-zhe-rui'),
+          ('twitter', 'https://twitter.com/lzrak47'),
+          )
+
