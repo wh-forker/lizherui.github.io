3829724db16430328063581c8ad123aa6f6990eb
lizherui <lzrak47m4a1@gmail.com>
2013-08-01 20:04:45 +0800

    ci by lizherui

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,7 +21,7 @@ DEFAULT_DATE_FORMAT = '%Y-%m-%d'
 
 GOOGLE_ANALYTICS = 'UA-42648273-1'
 
-DISQUS_SITENAME = 'lizherui'
+DISQUS_SITENAME = 'lizheruisworld'
 
 FEED_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
