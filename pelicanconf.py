2441377f239e8415a644bd589b263f603c944cc6
lizherui <lzrak47m4a1@gmail.com>
2013-08-01 16:37:19 +0800

    ci by lizherui

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,19 +4,28 @@ from __future__ import unicode_literals
 
 AUTHOR = u'lizherui'
 SITENAME = u"lizherui's blog"
+SITE_SOURCE = 'https://github.com/lizherui/lizherui.github.io'
 SITEURL = 'http://lizherui.github.io'
-TIMEZONE = 'Asia/Shanghai'
-THEME = "tuxlite_tbs"
-DEFAULT_LANG = u'zh'
-DEFAULT_DATE_FORMAT = '%Y-%m-%d'
-GITHUB_URL = "https://github.com/lizherui"
-GOOGLE_ANALYTICS = "UA-42648273-1"
-DISQUS_SITENAME = "lizherui"
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
-ARCHIVES_URL = "archives.html"
+ARCHIVES_URL = 'archives.html'
+GITHUB_URL = 'https://github.com/lizherui'
+
 RELATIVE_URLS = True
 DEFAULT_PAGINATION = 5
+
+TIMEZONE = 'Asia/Shanghai'
+
+THEME = 'tuxlite_tbs'
+
+DEFAULT_LANG = u'zh'
+
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
+GOOGLE_ANALYTICS = 'UA-42648273-1'
+
+DISQUS_SITENAME = 'lizherui'
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 # Blogroll
 LINKS =  (('Python.org', 'http://python.org/'),
