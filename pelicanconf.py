d7faeb02faef764c16ecb7c2e2f3150d5d87cd76
lizherui <lzrak47m4a1@gmail.com>
2013-08-17 14:45:58 +0800

    ci by lizherui

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = 'http://www.lizherui.com'
 GITHUB_URL = 'https://github.com/lizherui'
 ARCHIVES_URL = 'archives.html'
 ARTICLE_URL = 'posts/{slug}'
-ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
+ARTICLE_SAVE_AS = 'posts/{slug}.html'
 
 RELATIVE_URLS = True
 DEFAULT_PAGINATION = 5
